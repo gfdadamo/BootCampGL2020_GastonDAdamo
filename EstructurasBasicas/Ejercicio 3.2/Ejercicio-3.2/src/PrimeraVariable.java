405a503d75e3769972aacdfeba1c43d64003d7c8
Gaston <gastonfedericod@gmail.com>
2020-08-02 17:55:49 -0300

    Ejercicio 3.2

---
+++ EstructurasBasicas/Ejercicio 3.2/Ejercicio-3.2/src/PrimeraVariable.java
@@ -0,0 +1,16 @@
+
+public class PrimeraVariable {
+
+	public static void main(String[] args) {
+		
+		String mensaje="Valor inicial";
+		
+		System.out.println(mensaje);
+		
+		mensaje="Valor Modificado";
+		
+		System.out.println(mensaje);
+		
+	}
+
+}
