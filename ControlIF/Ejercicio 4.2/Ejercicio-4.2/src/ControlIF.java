52c61f3ba5a4355fece956d1d0736b02fe5fca26
Gaston <gastonfedericod@gmail.com>
2020-08-02 21:19:00 -0300

    Ejercicio 4.2

---
+++ ControlIF/Ejercicio 4.2/Ejercicio-4.2/src/ControlIF.java
@@ -0,0 +1,23 @@
+
+public class ControlIF {
+
+	public static void main(String[] args) {
+		
+		char letra='b';
+		
+
+		if(Character.isUpperCase(letra)) {
+			
+			 System.out.println("Es una letra Mayúscula");
+			
+		}
+		
+		else {
+               
+			System.out.println("No es una letra Mayúscula");
+			
+		}
+		
+	}
+
+}
