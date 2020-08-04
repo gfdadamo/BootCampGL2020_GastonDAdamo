a7d41755a929d1be78bca5b24d179326af5ae4af
Gaston <gastonfedericod@gmail.com>
2020-08-03 23:46:10 -0300

    Ejercicio 5.4

---
+++ PilaresPOO/Ejercicio 5.4/Ejercicio-5.4/src/Berlina.java
@@ -0,0 +1,17 @@
+
+public class Berlina extends Automovil {
+	
+	void avanzar() {
+		
+		System.out.println("Soy el metodo avanzar de Berlina");
+		
+		
+	}
+
+	@Override
+	void retroceder() {
+		
+		
+	}
+
+}
